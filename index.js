c7fa8c2f6ec8c0eba0ecfcad176c14c51aba71a8
kdrbipul <kdrbipul@gmail.com>
2023-06-17 16:57:39 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+// var num = 0.2;
+
+// if (num % 2 != 0){
+//     console.log("Odd");
+// }else{
+//     console.log ("Even");
+// }
+//  var num1 = 0;
+//   if (num1 > 0){
+//     console.log("Positive");
+//   }else if ( num1 < 0){
+//     console.log ("Negative");
+//   }else{
+//     console.log ("Zero");
+//   }
+var num = 109;
+
+if (num > 100 || num < 0){
+    console.log("Invalid Number");
+}else if (num >=80 && num <=100){
+    console.log("Letter grade A+");
+}else if (num >=70 && num <=79){
+    console.log("Letter grade A");
+}else if (num >=60 && num <=69){
+    console.log("Letter grade A-");
+}else if (num >=50 && num <=59){
+    console.log("Letter grade B");
+}else if (num >=40 && num <=49){
+    console.log("Letter grade C");
+}else if (num >=33 && num <=39){
+    console.log("Letter grade D");
+}else {
+    console.log("Letter grade Fail");
+}
+    
